07253781792ae2ce2e5712fecd3fb7c8e07903a4
Kelvin229 <kelvinchiodi@gmail.com>
2023-04-20 15:31:04 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
+
+// Serve static assets
+app.use(express.static('public'))
+
+app.listen(port, () => console.log('Server Started'))
