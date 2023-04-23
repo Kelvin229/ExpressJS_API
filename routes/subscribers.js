3be706b5d2d8f86d52f35c99a0104512784370fe
Kelvin229 <kelvinchiodi@gmail.com>
2023-04-23 10:12:18 -0400

    fixed async getSubscriber function

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -27,18 +27,16 @@ router.patch('/:id', getSubscriber, updateSubscriber);
 router.delete('/:id', getSubscriber, deleteSubcriber);
 
 async function getSubscriber(req, res, next) {
-  let subscriber
   try {
-    subscriber = await Subscriber.findById(req.params.id)
+    const subscriber = await Subscriber.findById(req.params.id);
     if (subscriber == null) {
-      return res.status(404).json({ message: 'Cannot find subscriber' })
+      return res.status(404).json({ message: 'Cannot find subscriber' });
     }
+    res.subscriber = subscriber;
+    next();
   } catch (err) {
-    return res.status(500).json({ message: err.message })
+    return res.status(500).json({ message: err.message });
   }
-
-  res.subscriber = subscriber
-  next()
 }
 
 module.exports = router
