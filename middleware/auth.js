6d1cd511dc13707d6d384d51c6711c2da1843301
Kelvin229 <kelvinchiodi@gmail.com>
2023-04-22 21:49:30 -0400

    fixed auth middleware issue

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,26 +3,32 @@ const jwt = require('jsonwebtoken');
 const secret = 'test';
 
 const auth = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(" ")[1];
-    const isCustomAuth = token.length < 500;
-
-    let decodedData;
-
-    if (token && isCustomAuth) {      
-      decodedData = jwt.verify(token, secret);
-
-      req.userId = decodedData?.id;
-    } else {
-      decodedData = jwt.decode(token);
-
-      req.userId = decodedData?.sub;
-    }    
-
-    next();
-  } catch (error) {
-    console.log(error);
-  }
-};
+    try {
+      const authHeader = req.headers.authorization;
+      if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+      
+      const token = authHeader.split(" ")[1];
+      const isCustomAuth = token.length < 500;
+  
+      let decodedData;
+  
+      if (token && isCustomAuth) {      
+        decodedData = jwt.verify(token, secret);
+  
+        req.userId = decodedData?.id;
+      } else {
+        decodedData = jwt.decode(token);
+  
+        req.userId = decodedData?.sub;
+      }    
+  
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Server error" });
+    }
+  };
 
 module.exports = auth;
