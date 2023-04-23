6b52ca3bfaaa877849829e272db9ff0343b6f3a6
Kelvin229 <kelvinchiodi@gmail.com>
2023-04-22 23:15:17 -0400

    secured server.js & added dependencies

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require("express-rate-limit");
+const { body } = require('express-validator');
 
 const port = process.env.PORT || 3000;
 
@@ -14,11 +17,20 @@ db.once('open', () => console.log('Connected to Database'))
 
 app.use(cors());
 app.use(express.json())
+app.use(helmet());
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+  });
+  app.use(limiter);
+  
 const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
+
 const postRoutes = require('./routes/posts')
 app.use('/posts', postRoutes)
+
 const userRouter = require('./routes/user')
 app.use('/user', userRouter)
 
