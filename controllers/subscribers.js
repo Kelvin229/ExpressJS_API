c11c8ece740e4dbfd279b9d14170ce4d09faa450
Kelvin229 <kelvinchiodi@gmail.com>
2023-04-23 09:48:16 -0400

    seperated subscriber routes from controller

---
+++ controllers/subscribers.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Subscriber = require('../models/subscriber')
+
+// Getting all
+const getAllSubscribers = async (req, res) => {
+    try {
+      const subscribers = await Subscriber.find()
+      res.json(subscribers)
+    } catch (err) {
+      res.status(500).json({ message: err.message })
+    }
+  }
+  
+  // Getting One
+  const getOneSubscriber =  (req, res) => {
+    res.json(res.subscriber)
+  }
+  
+  // Creating one
+  const createSubscriber = async (req, res) => {
+    const subscriber = new Subscriber({
+      name: req.body.name,
+      subscribedToChannel: req.body.subscribedToChannel
+    })
+    try {
+      const newSubscriber = await subscriber.save()
+      res.status(201).json(newSubscriber)
+    } catch (err) {
+      res.status(400).json({ message: err.message })
+    }
+  }
+  
+  // Updating One
+ const updateSubscriber = async (req, res) => {
+    if (req.body.name != null) {
+      res.subscriber.name = req.body.name
+    }
+    if (req.body.subscribedToChannel != null) {
+      res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+    }
+    try {
+      const updatedSubscriber = await res.subscriber.save()
+      res.json(updatedSubscriber)
+    } catch (err) {
+      res.status(400).json({ message: err.message })
+    }
+  }
+  
+  // Deleting One
+ const deleteSubcriber = async (req, res) => {
+    try {
+      await res.subscriber.remove()
+      res.json({ message: 'Deleted Subscriber' })
+    } catch (err) {
+      res.status(500).json({ message: err.message })
+    }
+  }
+
+  
+  const subscriberController = {
+    getAllSubscribers,
+    getOneSubscriber,
+    createSubscriber,
+    updateSubscriber,
+    deleteSubcriber,
+  };
+
+  module.exports = subscriberController;
--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,61 +1,29 @@
 const express = require('express')
+
+const {
+  getAllSubscribers,
+  getOneSubscriber,
+  createSubscriber,
+  updateSubscriber,
+  deleteSubcriber
+} = require('../controllers/subscribers.js');
+
 const router = express.Router()
-const Subscriber = require('../models/subscriber')
 
 // Getting all
-router.get('/', async (req, res) => {
-  try {
-    const subscribers = await Subscriber.find()
-    res.json(subscribers)
-  } catch (err) {
-    res.status(500).json({ message: err.message })
-  }
-})
+router.get('/', getAllSubscribers);
 
 // Getting One
-router.get('/:id', getSubscriber, (req, res) => {
-  res.json(res.subscriber)
-})
+router.get('/:id', getSubscriber, getOneSubscriber)
 
 // Creating one
-router.post('/', async (req, res) => {
-  const subscriber = new Subscriber({
-    name: req.body.name,
-    subscribedToChannel: req.body.subscribedToChannel
-  })
-  try {
-    const newSubscriber = await subscriber.save()
-    res.status(201).json(newSubscriber)
-  } catch (err) {
-    res.status(400).json({ message: err.message })
-  }
-})
+router.post('/', createSubscriber);
 
 // Updating One
-router.patch('/:id', getSubscriber, async (req, res) => {
-  if (req.body.name != null) {
-    res.subscriber.name = req.body.name
-  }
-  if (req.body.subscribedToChannel != null) {
-    res.subscriber.subscribedToChannel = req.body.subscribedToChannel
-  }
-  try {
-    const updatedSubscriber = await res.subscriber.save()
-    res.json(updatedSubscriber)
-  } catch (err) {
-    res.status(400).json({ message: err.message })
-  }
-})
+router.patch('/:id', getSubscriber, updateSubscriber);
 
 // Deleting One
-router.delete('/:id', getSubscriber, async (req, res) => {
-  try {
-    await res.subscriber.remove()
-    res.json({ message: 'Deleted Subscriber' })
-  } catch (err) {
-    res.status(500).json({ message: err.message })
-  }
-})
+router.delete('/:id', getSubscriber, deleteSubcriber);
 
 async function getSubscriber(req, res, next) {
   let subscriber
