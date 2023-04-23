c5ccec47e75aaded3d967a9edb35a78be72a05d3
Kelvin229 <kelvinchiodi@gmail.com>
2023-04-23 08:40:19 -0400

    properly defined endpoint schema in SwaggerUI

--- routes/user.js
+++ routes/user.js
@@ -14,8 +14,9 @@ const { signin, signup } = require('../controllers/user.js');
  * @swagger
  * /auth/signin:
  *   post:
- *     summary: Sign in a user.
- *     tags: [Authentication]
+ *     summary: Sign in a user
+ *     tags:
+ *       - auth
  *     requestBody:
  *       required: true
  *       content:
@@ -23,34 +24,116 @@ const { signin, signup } = require('../controllers/user.js');
  *           schema:
  *             $ref: '#/components/schemas/UserCredentials'
  *     responses:
- *       200:
- *         description: A token for the authenticated user.
+ *       '200':
+ *         description: A successful response with the user's details and a JWT token
  *         content:
  *           application/json:
  *             schema:
  *               $ref: '#/components/schemas/AuthToken'
+ *       '400':
+ *         description: Invalid request body or missing email/password fields
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Missing email or password
+ *       '401':
+ *         description: Failed login attempts exceeded for the email address
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Too many login attempts
+ *       '404':
+ *         description: User with specified email address not found
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: User doesn't exist
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Something went wrong
  */
 router.post("/signin", signin);
 
 /**
  * @swagger
- * /auth/signup:
+ * /signup:
  *   post:
- *     summary: Sign up a new user.
- *     tags: [Authentication]
+ *     summary: Register a new user
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             $ref: '#/components/schemas/NewUser'
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *             required:
+ *               - email
+ *               - password
+ *               - firstName
+ *               - lastName
  *     responses:
- *       200:
- *         description: A token for the new user.
+ *       201:
+ *         description: Created
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/AuthToken'
+ *               type: object
+ *               properties:
+ *                 result:
+ *                   $ref: '#/components/schemas/User'
+ *                 token:
+ *                   type: string
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ */
+ 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *         email:
+ *           type: string
+ *         name:
+ *           type: string
+ *         password:
+ *           type: string
+ *       required:
+ *         - _id
+ *         - email
+ *         - name
+ *         - password
  */
 router.post("/signup", signup);
 
